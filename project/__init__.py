29ad3803160763edf4d850c2094ebe402c2c2200
Loi Van Tran <datloiboi@gmail.com>
2023-01-18 16:21:13 -0500

    Clean code

--- project/__init__.py
+++ project/__init__.py
@@ -7,16 +7,10 @@ app.config.from_object('config')
 from project.models import User
 from project.db import db
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, Flask World!</p>"
-
 @app.route("/ticket", methods=['GET', 'POST'])
 def create_ticket():
     if request.method == 'POST':
-        print('Create Ticket')
         data = request.get_json()
-        print(data)
 
         u = User(body=data['text'], username=data['bar'])
         db.session.add(u)
